b0e81fb80697d73e0b8e33cb7d3de1a610c43818
FirrBotzz <97344921+Firaachanzz@users.noreply.github.com>
2022-02-28 03:38:15 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -5,9 +5,9 @@ const chalk = require('chalk')
 global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="Riych-Uhuy"
-global.ownernumber = "6281575886399"
-global.botname = "Riychbot"
+global.ownername ="FiraaChann"
+global.ownernumber = "6283830815715"
+global.botname = "BotzFiraaChan"
 global.thumbnail = fs.readFileSync("./settings/Riych.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'RIFQIBOTZ' //apikey
